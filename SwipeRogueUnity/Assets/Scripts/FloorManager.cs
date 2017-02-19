8a349d4a89052ce28f3610f900068eeb852d9761
Ben Wilcox <reccanti@gmail.com>
2017-02-19 00:17:52 -0500

    Create Game Manager
    Moves the Floor Manager into the Game Manager, and creates
    the Game Manager to handle more basic coordination

--- SwipeRogueUnity/Assets/Scripts/FloorManager.cs
+++ SwipeRogueUnity/Assets/Scripts/FloorManager.cs
@@ -7,24 +7,26 @@ public class FloorManager : MonoBehaviour {
 	public GameObject[] rooms;
 
 	/**
-	 * When the program wakes up. This may eventually
-	 * be replaced with a GameManager, but it's used for
-	 * testing right now
-	 */
-	public void Awake() {
-		SetupFloor ();
-	}
-
-	/**
 	 * A function that will render the floor
 	 */
 	public void SetupFloor () {
+		
 		float camWidth = Camera.main.orthographicSize * Screen.width / Screen.height;
 		float camHeight = Camera.main.orthographicSize;
 		GameObject centerRoom = Instantiate (rooms [0], new Vector3(0f, 0f, 0f), Quaternion.identity) as GameObject;
 		GameObject eastRoom = Instantiate (rooms [1], new Vector3 (2 * camWidth, 0f, 0f), Quaternion.identity) as GameObject;
 		GameObject westRoom = Instantiate (rooms [2], new Vector3 (-2 * camWidth, 0f, 0f), Quaternion.identity) as GameObject;
-		GameObject northRoom = Instantiate (rooms [3], new Vector3 (0f, -2 * camHeight, 0f), Quaternion.identity) as GameObject;
-		GameObject southRoom = Instantiate (rooms [4], new Vector3 (0f, 2 * camHeight, 0f), Quaternion.identity) as GameObject;
+		GameObject northRoom = Instantiate (rooms [3], new Vector3 (0f, 2 * camHeight, 0f), Quaternion.identity) as GameObject;
+		GameObject southRoom = Instantiate (rooms [4], new Vector3 (0f, -2 * camHeight, 0f), Quaternion.identity) as GameObject;
+
+		centerRoom.GetComponent<Room> ().East = eastRoom;
+		centerRoom.GetComponent<Room> ().West = westRoom;
+		centerRoom.GetComponent<Room> ().North = northRoom;
+		centerRoom.GetComponent<Room> ().South = southRoom;
+
+		northRoom.GetComponent<Room> ().South = centerRoom;
+		southRoom.GetComponent<Room> ().North = centerRoom;
+		eastRoom.GetComponent<Room> ().West = centerRoom;
+		westRoom.GetComponent<Room> ().East = centerRoom;
 	}
 }
---
+++ SwipeRogueUnity/Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager instance = null;
+	public FloorManager floorScript;
+
+	void Awake () {
+		if (instance == null)
+			instance = this;
+		else if (instance != this)
+			Destroy (gameObject);
+
+		DontDestroyOnLoad (gameObject);
+		floorScript = GetComponent<FloorManager> ();
+		floorScript.SetupFloor ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
