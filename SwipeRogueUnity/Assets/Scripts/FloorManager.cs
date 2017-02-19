1846841b12133bbb7ee329fed1869a5df40d9d26
Ben Wilcox <reccanti@gmail.com>
2017-02-18 21:49:55 -0500

    Lay out floor
    Lays out a simple floor with 5 rooms: 1 in the center, and one in
    each cardinal direction

---
+++ SwipeRogueUnity/Assets/Scripts/FloorManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorManager : MonoBehaviour {
+
+	public GameObject[] rooms;
+
+	/**
+	 * When the program wakes up. This may eventually
+	 * be replaced with a GameManager, but it's used for
+	 * testing right now
+	 */
+	public void Awake() {
+		SetupFloor ();
+	}
+
+	/**
+	 * A function that will render the floor
+	 */
+	public void SetupFloor () {
+		float camWidth = Camera.main.orthographicSize * Screen.width / Screen.height;
+		float camHeight = Camera.main.orthographicSize;
+		GameObject centerRoom = Instantiate (rooms [0], new Vector3(0f, 0f, 0f), Quaternion.identity) as GameObject;
+		GameObject eastRoom = Instantiate (rooms [1], new Vector3 (2 * camWidth, 0f, 0f), Quaternion.identity) as GameObject;
+		GameObject westRoom = Instantiate (rooms [2], new Vector3 (-2 * camWidth, 0f, 0f), Quaternion.identity) as GameObject;
+		GameObject northRoom = Instantiate (rooms [3], new Vector3 (0f, -2 * camHeight, 0f), Quaternion.identity) as GameObject;
+		GameObject southRoom = Instantiate (rooms [4], new Vector3 (0f, 2 * camHeight, 0f), Quaternion.identity) as GameObject;
+	}
+}
---
+++ SwipeRogueUnity/Assets/Scripts/Room.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room : MonoBehaviour {
+
+	public GameObject North;
+	public GameObject South;
+	public GameObject East;
+	public GameObject West;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
