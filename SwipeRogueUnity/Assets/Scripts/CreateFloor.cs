f96a12230d93888a0e827067aff9477c1871eb7f
Zachary Wilken <zxw8994@rit.edu>
2017-02-21 00:05:38 -0500

    added unfinished room generation scripts

---
+++ SwipeRogueUnity/Assets/Scripts/CreateFloor.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateFloor : MonoBehaviour {
+
+    public Transform[] spawnLocations;
+    public GameObject RoomPrefab;
+    public GameObject RoomClone;
+
+    private List<Room> roomList;
+    public int numOfRooms = 10;
+    private int roomsMade;
+
+
+    public string[] roomTypes;
+
+    
+
+    // Use this for initialization
+    private void Start () {
+
+        CreateRooms();
+
+
+	}
+	
+    // Each Room is created with open slots for adjacent rooms before they are added in
+
+    void CreateRooms()
+    {
+        // 1: bottom   2: Left   3: Top   4: Right
+
+
+
+        //room = new Room[roomNum];
+       // room = new List<Room>();
+       //
+       //
+       // room[0] = new Room(0, "entrance");
+       //
+       // room[0].BuildRoom(1);    //,"entrance");
+       //
+        for (int i = 0; i < numOfRooms; i++)
+        {
+
+            RoomClone = Instantiate(RoomPrefab,transform.position, Quaternion.Euler(0, 0, 0)) as GameObject;
+
+       //     // need to a way to track how many rooms are being made
+       //     room[i] = new Room(i,room[i-1], roomTypes[UnityEngine.Random.Range(1, 4)]);
+       //
+       //    // room[i].BuildRoom(UnityEngine.Random.Range(1, 3));  // ,roomTypes[UnityEngine.Random.Range(1, 4)]);
+        }
+
+
+    }
+
+}
---
+++ SwipeRogueUnity/Assets/Scripts/Room.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room : MonoBehaviour {
+
+
+    //private int adjRooms;
+    public GameObject RRoom = null;
+
+    public List<Room> connectedRooms;
+
+    protected int num;
+    public Room parent;
+    public string roomType;
+
+
+	
+	public Room(int num, Room parent, string type)
+    {
+        this.num = num;
+        this.parent = parent;
+        roomType = type;
+    }
+    public Room(int num, string type)
+    {
+        this.num = num;
+        roomType = type;
+    }
+
+    // Floor starting room
+    public void BuildRoom(int adjRooms )                                          //,string rmType)
+    {
+
+        
+        
+
+        //roomType = rmType;
+
+        /*switch (roomType)
+        {
+            case "entrance":
+                break;
+            case "empty":
+                break;
+            case "enemy":
+                break;
+            case "treasure":
+                break;
+            case "fountain":
+                break;
+            case "exit":
+                break;
+            default: // "exit"
+                break;
+        }*/
+    }
+
+
+
+    void GetAdjRooms()
+    {
+
+    }
+
+    void setAdjRooms()
+    {
+
+    }
+
+
+}
