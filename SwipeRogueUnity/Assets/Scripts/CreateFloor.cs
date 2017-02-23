19cacf7c5feba9f37b0c8761c1b98821ac3e1f61
Ben Wilcox <reccanti@gmail.com>
2017-02-22 23:08:35 -0500

    Create connected graph
    Creates a graph where all points are connected
    both ways. This does not account for potential overlap

--- SwipeRogueUnity/Assets/Scripts/CreateFloor.cs
+++ SwipeRogueUnity/Assets/Scripts/CreateFloor.cs
@@ -19,7 +19,7 @@ public enum Direction {
  * This may later be replaced with a MonoBehavior game object
  */
 public class RoomClass {
-	private Dictionary<Direction, RoomClass> neighbors;
+	public Dictionary<Direction, RoomClass> neighbors;
 	private int number;
 	private RoomClass parent;
 
@@ -40,7 +40,7 @@ public class RoomClass {
 	 */
 	public bool HasFreeEdges() {
 		foreach (Direction direction in Direction.GetValues(typeof(Direction))) {
-			if (this.neighbors [direction] != null) {
+			if (this.neighbors [direction] == null) {
 				return true;
 			}
 		}
@@ -50,7 +50,7 @@ public class RoomClass {
 	/**
 	 * Get a random direction that doesn't have a room next to it
 	 */
-	public Direction getRandomFreeDirection() {
+	public Direction GetRandomFreeDirection() {
 
 		// create a list of directions, and randomly remove values from it
 		// until we find a neighbor in the direction that is unoccupied
@@ -64,7 +64,6 @@ public class RoomClass {
 				directionList.RemoveAt(randomIndex);
 			}
 		}
-//		List<Direction> directionList = new List<Direction> (Direction.GetValues (typeof(Direction)));
 		return directionList[0];
 	}
 
@@ -106,11 +105,63 @@ public class CreateFloor : MonoBehaviour {
 
     // Use this for initialization
     private void Start () {
+		
+		// initialize the list of rooms with a single parent room
 		rooms = new List<RoomClass> ();
-		RoomClass r = new RoomClass (1, null);
-		for (int i = 0; i < 10; i++) {
-			Debug.Log(r.getRandomFreeDirection());
+		rooms.Add(new RoomClass(0, null));
+
+		// randomly add 10 connected rooms to the floor
+		for (int i = 1; i < 10; i++) {
+
+			// get the old node and the new node we will be linking to it
+			RoomClass newParent = GetRandomRoomWithFreeNeighbors ();
+			RoomClass newRoom = new RoomClass (i, newParent);
+
+			// get the directions so we can bind the rooms both ways
+			Direction direction = newParent.GetRandomFreeDirection ();
+			Direction oppositeDirection;
+			if (direction == Direction.North) 
+				oppositeDirection = Direction.South;
+			else if (direction == Direction.South)
+				oppositeDirection = Direction.North;
+			else if (direction == Direction.West)
+				oppositeDirection = Direction.East;
+			else
+				oppositeDirection = Direction.West;
+
+			// link the two nodes both ways
+			newParent.neighbors[direction] = newRoom;
+			newRoom.neighbors [oppositeDirection] = newParent;
+
+			// add the new room to the list so we can search again
+			rooms.Add(newRoom);
 		}
+
+		// test that this worked
+		foreach (RoomClass r in rooms) {
+			Debug.Log (r.ToString ());
+		}
+	}
+
+	/**
+	 * A function to randomly get a room with free edges
+	 */ 
+	private RoomClass GetRandomRoomWithFreeNeighbors() {
+
+		// create a shallow copy of the list so that we don't remove actual values
+		List<RoomClass> shuffleRooms = this.rooms.GetRange (0, this.rooms.Count);
+
+		// randomly remove rooms from the list
+		// until we find one with free edges
+		while (shuffleRooms.Count > 0) {
+			int randomIndex = Random.Range (0, shuffleRooms.Count);
+			if (shuffleRooms [randomIndex].HasFreeEdges ()) {
+				return shuffleRooms [randomIndex];
+			} else {
+				shuffleRooms.RemoveAt (randomIndex);
+			}
+		}
+		return null;
 	}
 //		
 //    // Each Room is created with open slots for adjacent rooms before they are added in
