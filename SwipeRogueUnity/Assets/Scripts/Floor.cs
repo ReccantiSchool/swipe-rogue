773a2c0dfe3616ef9deb640750fe8bcacc178e6c
Ben Wilcox <reccanti@gmail.com>
2017-04-19 15:47:15 -0400

    Spawn Rooms in branching graph
    Rooms will now spawn randomly in a branching graph. Overlaps are
    still common though. This can be fixed by moving neighbor
    functionality from the Room script to the floor script. The floor
    will be responsible for keeping track of the grid, while the
    room will just store references to it's neighbors for the purpose
    of movement

---
+++ SwipeRogueUnity/Assets/Scripts/Floor.cs
@@ -0,0 +1,138 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Floor : MonoBehaviour {
+
+	public Dictionary<Vector2, GameObject> rooms = new Dictionary<Vector2, GameObject>();
+	public GameObject roomPrefab;
+
+	// references to the camera dimensions that will be calulated once
+	private float camWidth;
+	private float camHeight;
+
+
+	// Use this for initialization
+	void Start () {
+		camWidth = Camera.main.orthographicSize * Screen.width / Screen.height;
+		camHeight = Camera.main.orthographicSize;
+		CreateRandomFloor(10);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void CreateRandomFloor(int roomCount) {
+
+		// initialize the rooms list with a single parent node
+		AddRoomAtLocation(Vector2.zero);
+
+		// cycle through the total number of rooms wanted and add them to the list
+		for (int i = 0; i < roomCount - 1; i++) {
+			
+			// get a random room with a free neighbor
+			// and make sure that it doesn't conflict with another neighbor
+			Vector2? parentRoomLocation = GetRandomRoomLocationWithFreeNeighbors();
+			Debug.Log(parentRoomLocation);
+			if (parentRoomLocation != null) {
+				Vector2 indexLocation = parentRoomLocation ?? default(Vector2);
+				Room parentRoom = rooms[indexLocation].GetComponent<Room>();
+
+				// instantiate the Room at the given transform
+				Direction? adjacentDirection = parentRoom.GetRandomFreeDirection();
+				Vector2 newLocation;
+				switch (adjacentDirection) {
+					case Direction.North:
+						newLocation = indexLocation + Vector2.down;
+						break;
+					case Direction.South:
+						newLocation = indexLocation + Vector2.up;
+						break;
+					case Direction.East:
+						newLocation = indexLocation + Vector2.right;
+						break;
+					default:
+						newLocation = indexLocation + Vector2.left;
+						break;
+				}
+				GameObject newRoom = AddRoomAtLocation(newLocation);
+
+			}
+
+			
+			// bind it both ways
+		}
+	}
+
+	/**
+	 * A function that initializes a room at a given location
+	 */
+	private GameObject AddRoomAtLocation(Vector2 gridPosition) {
+		Vector3 position = new Vector3(gridPosition.x * camWidth * 2, gridPosition.y * camHeight * 2, 0);
+		GameObject room = Instantiate(roomPrefab, position, Quaternion.identity);
+		room.transform.parent = transform;
+		rooms.Add(gridPosition, room);
+		return room;
+	}
+
+	/**
+	 * A function that randomly gets a room location with at least one free edge.
+	 * If no rooms in the list have a free edge, this function returns null
+	 */
+	private Vector2? GetRandomRoomLocationWithFreeNeighbors() {
+
+		// make a list of all of the room locations in dictionary
+		List<Vector2> shuffleLocations = new List<Vector2>(this.rooms.Keys);
+
+		// cycle through the room locations until we find one one with a free edge
+		while (shuffleLocations.Count > 0) {
+
+			// get a room at a random index
+			int randomIndex = Random.Range (0, shuffleLocations.Count);
+			Room randomRoom = rooms[shuffleLocations[randomIndex]].GetComponent<Room>();
+
+			// return the room location if it has a free edge. Otherwise remove it and iterate the loop
+			if (randomRoom.HasFreeEdges()) {
+				return shuffleLocations[randomIndex];
+			} else {
+				shuffleLocations.RemoveAt(randomIndex);
+			}
+		}
+
+		// return null if no free rooms were found
+		return null;
+	}
+
+	/**
+	 * Returns a list of all adjacent rooms to a position in the grid
+	 */
+	private List<Direction> GetAllAdjacentDirections(Vector2 gridPostion) {
+		List<Direction> adjacentDirection = new List<Direction>();
+		GameObject defaultRoom;
+
+		// test the north room
+		if (rooms.TryGetValue(gridPostion + Vector2.down, out defaultRoom)) {
+			adjacentDirection.Add(Direction.North);
+		}
+		// test the south room
+		if (rooms.TryGetValue(gridPostion + Vector2.up, out defaultRoom)) {
+			adjacentDirection.Add(Direction.South);
+		}
+		// test the east room
+		if (rooms.TryGetValue(gridPostion + Vector2.right, out defaultRoom)) {
+			adjacentDirection.Add(Direction.East);
+		}
+		// test the west room
+		if (rooms.TryGetValue(gridPostion + Vector2.left, out defaultRoom)) {
+			adjacentDirection.Add(Direction.West);
+		}
+
+		return adjacentDirection;
+	}
+
+	private void BindRooms(GameObject fromRoom, GameObject toRoom, Direction inDirection) {
+
+	}
+}
--- SwipeRogueUnity/Assets/Scripts/Room.cs
+++ SwipeRogueUnity/Assets/Scripts/Room.cs
@@ -6,179 +6,53 @@ public class Room : MonoBehaviour {
 
     public RoomClass roomclass;
 
-    [HideInInspector]
     public GameObject NorthRoom;
 
-    [HideInInspector]
     public GameObject SouthRoom;
 
-    [HideInInspector]
     public GameObject EastRoom;
 
-    [HideInInspector]
     public GameObject WestRoom;
 
-    // public Dictionary<Direction, GameObject> neighbors { get; set; }
-    // public int number { get; set; }
+    /**
+     * Check to see if this room has a free edge
+     */
+    public bool HasFreeEdges() {
+        if (NorthRoom == null ||
+            SouthRoom == null ||
+            EastRoom  == null ||
+            WestRoom  == null) {
+            return true;
+        }
+        return false;
+    }
 
-    // public GameObject parent { get; set; }
+    /**
+     * Returns a random free adjacent direction to the room
+     */
+    public Direction? GetRandomFreeDirection() {
 
-    // public int x { get; set; }
-
-    // public int y { get; set; }
-
-
-    // //private int adjRooms;
-    // public GameObject Room = null;
-
-    // public List<Room> connectedRooms;
-
-    // protected int num;
-    // public Room parent;
-    // public string roomType;
-
-
-	
-	// public Room(int num, Room parent, string type)
-    // {
-    //     this.num = num;
-    //     this.parent = parent;
-    //     roomType = type;
-    // }
-    // public Room(int num, string type)
-    // {
-    //     this.num = num;
-    //     roomType = type;
-    // }
-
-    // // Floor starting room
-    // public void BuildRoom(int adjRooms )                                          //,string rmType)
-    // {
-
+        // generate a list of free adjacent directions
+        List<Direction> freeDirections = new List<Direction>();
+        if (NorthRoom == null) {
+            freeDirections.Add(Direction.North);
+        }
+        if (SouthRoom == null) {
+            freeDirections.Add(Direction.South);
+        }
+        if (EastRoom == null) {
+            freeDirections.Add(Direction.East);
+        }
+        if (WestRoom == null) {
+            freeDirections.Add(Direction.West);
+        }
         
-        
-
-    //     //roomType = rmType;
-
-    //     /*switch (roomType)
-    //     {
-    //         case "entrance":
-    //             break;
-    //         case "empty":
-    //             break;
-    //         case "enemy":
-    //             break;
-    //         case "treasure":
-    //             break;
-    //         case "fountain":
-    //             break;
-    //         case "exit":
-    //             break;
-    //         default: // "exit"
-    //             break;
-    //     }*/
-    // }
-
-
-
-    // void GetAdjRooms()
-    // {
-
-    // }
-
-    // void setAdjRooms()
-    // {
-
-    // }
-
+        // return a random free adjacent direction or null if none exist
+        if (freeDirections.Count > 0) {
+            int freeIndex = Random.Range(0, freeDirections.Count);
+            return freeDirections[freeIndex];
+        }
+        return null;
+    }
 
 }
-
-
-
-// /**
-//  * This class holds data related to Rooms in the floor graph.
-//  * This may later be replaced with a MonoBehavior game object
-//  */
-// public class RoomClass {
-// 	public Dictionary<Direction, RoomClass> neighbors;
-// 	public int number;
-// 	private RoomClass parent;
-//     public int x;
-//     public int y;
-
-//     /**
-// 	 * Instantiate a room with a number and it's parent
-// 	 */
-//     public RoomClass(int number, RoomClass parent) {
-// 		this.number = number;
-// 		this.parent = parent;
-//         x = parent.x;
-//         y = parent.y;
-// 		this.neighbors = new Dictionary<Direction, RoomClass>();
-// 		foreach (Direction direction in Direction.GetValues(typeof(Direction))) {
-// 			this.neighbors.Add(direction, null);
-// 		}
-// 	}
-//     public RoomClass(int number, RoomClass parent,int x,int y)
-//     {
-//         this.number = number;
-//         this.parent = parent;
-//         this.x = x;
-//         this.y = y;
-//         this.neighbors = new Dictionary<Direction, RoomClass>();
-//         foreach (Direction direction in Direction.GetValues(typeof(Direction)))
-//         {
-//             this.neighbors.Add(direction, null);
-//         }
-//     }
-// 	/**
-// 	 * Check to see if the room has a free edge
-// 	 */
-// 	public bool HasFreeEdges() {
-// 		foreach (Direction direction in Direction.GetValues(typeof(Direction))) {
-// 			if (this.neighbors [direction] == null) {
-// 				return true;
-// 			}
-// 		}
-// 		return false;
-// 	}
-
-// 	/**
-// 	 * Get a random direction that doesn't have a room next to it
-// 	 */
-// 	public Direction GetRandomFreeDirection() {
-
-// 		// create a list of directions, and randomly remove values from it
-// 		// until we find a neighbor in the direction that is unoccupied
-// 		Direction[] directions = (Direction[])Direction.GetValues(typeof(Direction));
-// 		List<Direction> directionList = new List<Direction> (directions);
-// 		while (directionList.Count > 1) {
-// 			int randomIndex = Random.Range (0, directionList.Count);
-// 			if (this.neighbors [directionList[randomIndex]] == null) {
-// 				return directionList [randomIndex];
-// 			} else {
-// 				directionList.RemoveAt(randomIndex);
-// 			}
-// 		}
-// 		return directionList[0];
-// 	}
-
-// 	/**
-// 	 * Print all of the properties of the Room object
-// 	 */
-// 	public override string ToString ()
-// 	{
-// 		string roomString = string.Format ("ID: {0}\n", this.number);
-// 		roomString += (this.parent == null) ? string.Format ("Parent: null\n") : string.Format ("Parent: {0}\n", this.parent.number);
-// 		roomString += string.Format("({0}, {1})", this.x, this.y);
-// 		roomString += "Neighbors:\n"; 
-// 		foreach (Direction direction in Direction.GetValues(typeof(Direction))) {
-// 			RoomClass directionRoom = this.neighbors [direction];
-// 			roomString += (directionRoom == null) ? string.Format ("\t{0}: null\n", direction.ToString()) : string.Format ("\t{0}: {1}\n", direction.ToString(), directionRoom.number);
-// 		}
-// 		return roomString;
-// 	}
-
-
-// }
