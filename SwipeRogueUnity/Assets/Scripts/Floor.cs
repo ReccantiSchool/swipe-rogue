6ccdbb065e5e5c278babb785cff786d636e249c3
Ben Wilcox <reccanti@gmail.com>
2017-04-20 19:47:26 -0400

    Revert "Revert "Custom Drawer for Room Dictionary""
    This reverts commit 85ce28b9f1d4bc8da5d964e5b4abd7f6474e6c99.

--- SwipeRogueUnity/Assets/Scripts/Floor.cs
+++ SwipeRogueUnity/Assets/Scripts/Floor.cs
@@ -2,10 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class Vector2GameObjectDictionary: SerializableDictionary<Vector2, GameObject> {}
+
+// [CreateAssetMenu(menuName = "Floor")]
 public class Floor : MonoBehaviour {
 
 	// a dictionary with references to all the rooms and their locations
-	public Dictionary<Vector2, GameObject> rooms = new Dictionary<Vector2, GameObject>();
+	[SerializeField]
+	public Vector2GameObjectDictionary roomsStore = new Vector2GameObjectDictionary();
+	public Dictionary<Vector2, GameObject> rooms {
+		get { return roomsStore.dictionary; }
+	}
 
 	// the prefab that will be used to render a room
 	public GameObject roomPrefab;
@@ -25,6 +33,9 @@ public class Floor : MonoBehaviour {
 		
 	}
 
+	/**
+	 * a method that can be called to initialize the floor manually
+	 */
 	public void InitializeFloor() {
 		camWidth = Camera.main.orthographicSize * Screen.width / Screen.height;
 		camHeight = Camera.main.orthographicSize;
